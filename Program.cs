7f73141226c1b2d37a41328a89d14430389ca3c8
PIPOAUMPHOL <aumpholllo@gmail.com>
2024-01-25 09:37:51 +0700

    feat: add completed find the average app

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace AvgApp {
+    class Program{
+        static void Main() {
+        
+        int sum = 0;
+        int avg = 0;
+
+        for (int count = 1; count <=5; count++) {
+            int number;
+            Console.Write("Input Number = ");
+            int.TryParse(Console.ReadLine(), out number);
+            sum = sum + number;
+            avg = avg + 1;
+        }
+
+
+        Console.WriteLine((double)sum/avg);
+
+        
+        }
+    }
+}
